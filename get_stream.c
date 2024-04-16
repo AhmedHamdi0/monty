c46b3cbb6a09ff903cdda403d003e648b0546758
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-16 23:18:13 +0200

    add docs for `getting_stream_failed` and `get_stream`

--- get_stream.c
+++ get_stream.c
@@ -1,21 +1,36 @@
 #include "monty.h"
 
+/**
+ * getting_stream_failed - Handles the case of failed file opening
+ * @filename: The name of the file that failed to open
+ *
+ * This function prints an error message indicating that a file couldn't be
+ * opened and exits the program with failure.
+ */
 void getting_stream_failed(char *filename)
 {
 	dprintf(2, "Error: can't open file %s\n", filename);
-//	free_arguments();
+	free_arguments();
 	exit(EXIT_FAILURE);
 }
 
+/**
+ * get_stream - Opens a file stream for reading
+ * @filename: The name of the file to open
+ *
+ * This function opens a file stream for reading and associates it with the
+ * global arguments structure.
+ */
 void get_stream(char *filename)
 {
 	int file_descriptor;
+
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 		getting_stream_failed(filename);
 
 	arguments->stream = fdopen(file_descriptor, "r");
-	if(arguments == NULL)
+	if (arguments->stream == NULL)
 	{
 		close(file_descriptor);
 		getting_stream_failed(filename);
