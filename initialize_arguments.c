3f21617ff11eb0def793148410b3dae9263d890c
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-16 23:11:48 +0200

    Create initialize_arguments.c

---
+++ initialize_arguments.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * initialize_arguments - Initializes the global arguments
+ * which is a pointer to arg_t struct.
+ *
+ * This function allocates memory for the global arguments structure
+ * and initializes its members to NULL.
+ */
+void initialize_arguments(void)
+{
+	arguments = malloc(sizeof(arg_t));
+	if (arguments == NULL)
+		malloc_failed();
+
+	arguments->stream = NULL;
+	arguments->line = NULL;
+}
