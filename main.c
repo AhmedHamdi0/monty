5395ff1fe161ca36fed573022e07c4617b51370a
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-16 21:48:23 +0200

    implement simple `main.c` program

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+#include "monty.h"
 
-int main() {
-	printf("Hello, World!\n");
-	return 0;
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: A pointer to an array of strings containing
+ * the command-line arguments
+ * Return: returns 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	if (argc != 2)
+	{
+		dprintf(2, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return (0);
 }
