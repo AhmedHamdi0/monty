f5a8abc890f4363cfc850a2e66ed9103ec1f0927
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-17 04:24:27 +0200

    Update get_instruction.c

--- get_instruction.c
+++ get_instruction.c
@@ -11,9 +11,7 @@ void invalid_instruction(void)
 {
 	dprintf(2, "L%d: unknown instruction %s\n",
 			arguments->line_number, arguments->tokens[0]);
-	close_stream();
-	free_tokens();
-	free_arguments();
+	free_all_args();
 	exit(EXIT_FAILURE);
 }
 
