f79d8627aa8f8046972a8554269ddf164cd156e8
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-17 04:22:54 +0200

    implement `free_stack` function

--- deallocators/free_stack.c
+++ deallocators/free_stack.c
@@ -1,14 +1,18 @@
-//#include "monty.h"
-//
-//void free_stack(stack_t *head)
-//{
-//	if (head == NULL)
-//		return;
-//
-//	if (head->next != NULL)
-//	{
-//		free_stack(head->next);
-//	}
-//
-//	free(head);
-//}
+#include "../monty.h"
+
+/**
+ * free_stack - Frees a stack starting from the given head
+ * @head: Pointer to the head of the stack
+ */
+void free_stack(stack_t *head)
+{
+	if (head == NULL)
+		return;
+
+	if (head->next != NULL)
+	{
+		free_stack(head->next);
+	}
+
+	free(head);
+}
