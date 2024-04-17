facf3d6af8696aacd6a01e9b60e255320ee2f6e2
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-17 21:06:21 +0200

    update `get_instruction` to handle comments `#`

--- get_instruction.c
+++ get_instruction.c
@@ -37,6 +37,13 @@ void get_instruction(void)
 	if (arguments->n_tokens == 0)
 		return;
 
+	if (arguments->tokens[0][0] == '#')
+	{
+		arguments->instruction->opcode = "nop";
+		arguments->instruction->f = nop;
+		return;
+	}
+
 	for (; instructions[i].opcode != NULL; i++)
 	{
 		if (strcmp(instructions[i].opcode, arguments->tokens[0]) == 0)
