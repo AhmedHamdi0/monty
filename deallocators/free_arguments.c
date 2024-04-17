3bf058f5df0b97156a20d4e24179b95e4e6bef3b
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-17 04:22:34 +0200

    implement `free_arguments` function

--- deallocators/free_arguments.c
+++ deallocators/free_arguments.c
@@ -1,31 +1,26 @@
-//#include "monty.h"
-//
-//void free_head(void)
-//{
-//	if (arguments->head)
-//		free_stack(arguments->head);
-//
-//	arguments->head = NULL;
-//}
-//
-//void free_arguments()
-//{
-//	if (arguments == NULL)
-//		return;
-//
-//	if (arguments->instruction)
-//	{
-//		free(arguments->instruction);
-//		arguments->instruction = NULL;
-//	}
-//
-//	free_head();
-//
-//	if (arguments->line)
-//	{
-//		free(arguments->line);
-//		arguments->line = NULL;
-//	}
-//
-//	free(arguments);
-//}
+#include "../monty.h"
+
+/**
+ * free_arguments - Frees memory allocated for arguments
+ */
+void free_arguments(void)
+{
+	if (arguments == NULL)
+		return;
+
+	if (arguments->instruction)
+	{
+		free(arguments->instruction);
+		arguments->instruction = NULL;
+	}
+
+	free_head();
+
+	if (arguments->line)
+	{
+		free(arguments->line);
+		arguments->line = NULL;
+	}
+
+	free(arguments);
+}
