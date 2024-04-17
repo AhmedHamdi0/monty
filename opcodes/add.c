2c9e7f0268743e9f25adff745eeb70acee184567
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-17 19:32:21 +0200

    implement `add` opcode function

---
+++ opcodes/add.c
@@ -0,0 +1,26 @@
+#include "../monty.h"
+
+/**
+ * add - Adds the top two elements of the stack
+ * @stack: Pointer to the top of the stack
+ * @line_number: Line number being executed
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	stack_t *temp1, *temp2;
+
+	if (arguments->stack_length < 2)
+	{
+		dprintf(2, "L%d: can't add, stack too short\n", line_number);
+		free_all_args();
+		exit(EXIT_FAILURE);
+	}
+
+	temp1 = arguments->head;
+	temp2 = temp1->next;
+
+	temp2->n = temp1->n + temp2->n;
+	arguments->head = arguments->head->next;
+	arguments->stack_length -= 1;
+}
